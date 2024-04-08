ebcc2b43e2593e232ca7d1c9b47c647f806144c7
SumonKrishnaDas <sumondash51583@gmail.com>
2024-04-08 15:54:36 +0600

    navbar footer compleate

---
+++ src/Layout/Main.jsx
@@ -0,0 +1,21 @@
+import{Outlet} from'react-router-dom'
+
+import Footer from '../Pages/Shared/Footer/Footer';
+import Navbar from '../Pages/Navbar';
+
+
+
+
+const Main = () => {
+    return (
+        <div>
+
+            <Navbar></Navbar>
+            <Outlet> </Outlet>
+            <Footer> </Footer>
+
+        </div>
+    );
+};
+
+export default Main;
---
+++ src/main.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { RouterProvider,} from 'react-router-dom'
+import router from './Routes/Route.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+
+<div className='mx-7xl'  >  
+
+
+  <div className='max-w-7xl mx-auto  '>
+  <React.StrictMode>
+    <RouterProvider router={router}> </RouterProvider>
+  </React.StrictMode>
+
+  </div>
+
+
+</div>
+
+ 
+)
